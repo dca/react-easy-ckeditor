d5d18ee49d15ffd43e24cd25f7e9e63b9bf12b72
Dca <dcahsu@gmail.com>
2017-04-17 15:45:39 +0800

    feat(core): Add EasyCKEditor component

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-import Component from '../../src'
+import EasyCKEditor from '../../src'
 
-let Demo = React.createClass({
-  render() {
-    return <div>
-      <h1>react-easy-ckeditor Demo</h1>
-      <Component/>
-    </div>
+class Demo extends React.Component {
+  constructor (props) {
+    super(props)
+    this.onChange = this.onChange.bind(this)
   }
-})
+
+  onChange (name, textHTML) {
+    this.setState({ [name]: textHTML })
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>react-easy-ckeditor Demo</h1>
+        <EasyCKEditor
+          name='desc'
+          textHTML='<h1>Hello</h1>World'
+          onChange={this.onChange}
+        />
+      </div>
+    )
+  }
+}
 
 render(<Demo/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,62 @@
-import React from 'react'
+const React = require('react')
+const PropTypes = require('prop-types')
+const scriptjs = require('scriptjs')
+const debug = require('debug')('react-easy-ckeditor')
 
-export default React.createClass({
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+let CKEDITOR_URL = 'https://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.6.2/ckeditor.js'
+
+class EasyCKEditor extends React.Component {
+  constructor (props) {
+    super(props)
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.onChange = this.onChange.bind(this)
   }
-})
+
+  componentWillMount () {
+    scriptjs(CKEDITOR_URL, 'ckeditor')
+    debug('componentWillMount: load script')
+  }
+
+  componentDidMount () {
+    debug('componentDidMount: call `replace` onReady')
+    let _this = this
+    scriptjs.ready('ckeditor', () => {
+      _this._editor = window.CKEDITOR.replace(_this.props.name)
+      _this._editor.on('change', _this.onChange)
+      _this._editor.on('blur', _this.onChange)
+    })
+  }
+
+  onChange () {
+    let textHTML = this._editor.getData()
+
+    this.setState({ [this.props.name]: textHTML })
+    this.props.onChange(this.props.name, textHTML)
+  }
+
+  render () {
+    return (
+      <textarea
+        style={{display: 'none'}}
+        rows='10' cols='80'
+        id={this.props.name}
+        name={this.props.name}
+        defaultValue={unescape(this.props.textHTML)}
+      />
+    )
+  }
+}
+
+EasyCKEditor.propTypes = {
+  name: PropTypes.string,
+  textHTML: PropTypes.string,
+  onChange: PropTypes.func
+}
+
+EasyCKEditor.defaultProps = {
+  name: Math.random().toString(),
+  textHTML: '',
+  onChange: (name, value) => {}
+}
+
+export default EasyCKEditor
